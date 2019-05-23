3413bbc944986f2cfe7f0285bd21ebd30fa4b9dd
apaletz <apaletz@gmail.com>
2019-05-23 12:09:38 -0300

    added some comments

--- all_genres_analyzer.py
+++ all_genres_analyzer.py
@@ -4,51 +4,32 @@ import csv
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+#create initial dataframe from csv file
 tracks = pd.read_csv("all_genres_1.csv")
 
-# print(tracks)
-
-# print(tracks.Genre.unique())
-
+#create genres dataframe
 genres = tracks.Genre.unique()
 
-print(tracks.Artist)
+#a function to create bar charts that show a given creteria by genere
+def chart_maker(criteria):
+	for genre in genres:
+		genre_df = tracks.loc[tracks['Genre'] == genre]
+		print(genre)
 
-for genre in genres:
-	genre_df = tracks.loc[tracks['Genre'] == genre]
-	print(genre)
+		print(genre_df[criteria].value_counts())
+		criteriaArray = genre_df[criteria].value_counts()
 
+		bar = criteriaArray.plot.bar(title=genre + 'tracks by ' + criteria)
+		plt.show()
 
-	print(genre_df['Key'].value_counts())
-	# keyArray = genre_df['Key'].value_counts()
-	# print(str(keyArray.size())
-
-	keyArray = genre_df['Key'].value_counts()
-
-	bar = keyArray.plot.bar(title=genre + " tracks by key")
-
+#a function to create bar charts that show a given criteria for all tracks
+def all_by_criteria(criteria):
+	all_tracks = tracks[criteria].value_counts()
+	graph = all_tracks.plot.bar(title = "All tracks by " + criteria)
 	plt.show()
 
-for genre in genres:
-	genre_df = tracks.loc[tracks['Genre'] == genre]
-	print(genre)
+chart_maker('Key')
+all_by_criteria('Key')
 
-
-	print(genre_df['BPM'].value_counts())
-	# keyArray = genre_df['Key'].value_counts()
-	# print(str(keyArray.size())
-
-	keyArray = genre_df['BPM'].value_counts()
-
-	bar = keyArray.plot.bar(title=genre + " tracks by bpm")
-
-	plt.show()
-
-
-all_tracks_keys = tracks['BPM'].value_counts()
-
-print(all_tracks_keys)
-
-graph = all_tracks_keys.plot.bar(title="All tracks by BPM")
-
-plt.show()
+chart_maker('BPM')
+all_by_criteria('BPM')
